de5dd4779d2c9a5c117a9ff6e0528426aba47612
Marvin <m.arets@student.fontys.nl>
2019-04-10 13:23:04 +0200

    Changed WiFi tracker class

--- WifiTrackingCollector/WiFiTracker.cs
+++ WifiTrackingCollector/WiFiTracker.cs
@@ -1,23 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO.Ports;
 
 namespace WifiTrackingCollector
 {
-    class WiFiTracker
+    public class WiFiTracker
     {
         public int TrackerID { get; private set; }
         public int CoupeNr { get; private set; }
-        public int ComPort { get; private set; }
+        public string ComPort { get; set; }
+        public SerialPort Serial { get; set; }
 
         public WiFiTracker(int trackerID, int coupeNr)
         {
-
+            TrackerID = trackerID;
+            CoupeNr = coupeNr;
+            ComPort = "";
+            Serial = null;
         }
 
         public void Collect()
-        {
-
+        {l
+           
         }
 
         public override string ToString()
